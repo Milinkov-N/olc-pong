0b88d523358bb919596146e45083a33b23773339
Milinkov-N <milinkov.nik@gmail.com>
2022-09-12 14:01:30 +0400

    added virtual terminal and non-blocking input

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+
+// This needed because i have old SDK version
+#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+#endif
 
 #define HEIGHT 25
 #define WIDTH 81
@@ -22,16 +29,41 @@ typedef struct State {
   int racketB_pos;
 } state_t;
 
+void run_pong(state_t *state);
 state_t *init_state();
 void destroy_state(state_t *state);
+void enable_vterm();
+void hide_cursor();
+void show_cursor();
 void render(state_t *state);
+void clear_screen();
 int get_item_to_render(state_t *state, int row, int col);
 
 int main(void) {
   state_t *state = init_state();
-  render(state);
-  destroy_state(state);
+  enable_vterm();
+
+  if (state) {
+    hide_cursor();
+    run_pong(state);
+    show_cursor();
+    destroy_state(state);
+  }
+
   return 0;
+}
+
+void run_pong(state_t *state) {
+  while (1) {
+    render(state);
+    if (_kbhit()) {
+      char ch = _getch();
+      if (ch == 'q')
+        break;
+    }
+    Sleep(100);
+    clear_screen();
+  }
 }
 
 state_t *init_state() {
@@ -40,8 +72,8 @@ state_t *init_state() {
   if((state = malloc(sizeof(state_t)))) {
     state->playerA_score = 0;
     state->playerB_score = 0;
-    state->racketA_pos = H_EDGE / 2;
-    state->racketB_pos = H_EDGE / 2;
+    state->racketA_pos   = H_EDGE / 2;
+    state->racketB_pos   = H_EDGE / 2;
   }
 
   return state;
@@ -52,21 +84,46 @@ void destroy_state(state_t *state) {
     free(state);
 }
 
+void enable_vterm() {
+  HANDLE h_stdout;
+  DWORD stdout_mode = 0;
+
+  h_stdout = GetStdHandle(STD_OUTPUT_HANDLE);
+  GetConsoleMode(h_stdout, &stdout_mode);
+  stdout_mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+  SetConsoleMode(h_stdout, stdout_mode);
+}
+
+void hide_cursor() {
+  printf("\x1b[?25l");
+}
+
+void show_cursor() {
+  printf("\x1b[?25h");
+}
+
 void render(state_t *state) {
   for (int i = 0; i < HEIGHT; i++) {
+    char line_buf[WIDTH] = {0};
     for (int j = 0; j < WIDTH; j++) {
       switch (get_item_to_render(state, i, j)) {
-        case RI_BALL:         printf("o"); break;
-        case RI_RACKET_LEFT:  printf("]"); break;
-        case RI_RACKET_RIGHT: printf("["); break;
-        case RI_CORNER:       printf("+"); break;
-        case RI_HOR_LINE:     printf("-"); break;
-        case RI_VER_LINE:     printf("|"); break;
-        case RI_VOID:         printf(" "); break;
+        case RI_BALL:         line_buf[j] = 'o'; break;
+        case RI_RACKET_LEFT:  line_buf[j] = ']'; break;
+        case RI_RACKET_RIGHT: line_buf[j] = '['; break;
+        case RI_CORNER:       line_buf[j] = '+'; break;
+        case RI_HOR_LINE:     line_buf[j] = '-'; break;
+        case RI_VER_LINE:     line_buf[j] = '|'; break;
+        case RI_VOID:         line_buf[j] = ' '; break;
       }
     }
-    printf("\n");
+    printf("%s\r\n", line_buf);
   }
+}
+
+void clear_screen() {
+  for (int i = 0; i < HEIGHT; i++)
+    printf("\x1b[2K\x1b[1A");
+  printf("\r");
 }
 
 int get_item_to_render(state_t *state, int row, int col) {
